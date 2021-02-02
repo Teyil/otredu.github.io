b808ab1a586e6f4e63591273b6cb0b493d0cec01
Teyil <teyil97@gmail.com>
2021-02-02 11:50:13 +0200

    update

---
+++ fanikauppa/precache-manifest.9f60234fa3e027e452b9f87d6bf1f13c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97302a0fd9ed33dcac9aca405f3ab907",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "7c8759aa68e5d114bd86",
+    "url": "/fanikauppa/static/css/main.63500e61.chunk.css"
+  },
+  {
+    "revision": "372e1abf98c0888be859",
+    "url": "/fanikauppa/static/js/2.a35fd71d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fanikauppa/static/js/2.a35fd71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8759aa68e5d114bd86",
+    "url": "/fanikauppa/static/js/main.b63ddcd9.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fanikauppa/static/media/logo.ee7cd8ed.svg"
+  }
+]);
